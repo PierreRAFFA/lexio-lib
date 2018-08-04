c5f422fb0ed33eb41c75fea93014bd80bee44d0e
PierreRAFFA <pierre.raffa@gmail.com>
2018-08-04 22:29:35 +0100

    test

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -53,7 +53,7 @@ export function getJwt(req: LexioRequest): string {
  */
 export async function requestPromise(options: any): Promise<any> {
   return new Promise<any>(async (resolve, reject) => {
-    request.get;
+    // request.get;
     // return request.get(options, (error: any, response: Response, body: any): void => {
     //   const statusCode = get(response, 'statusCode') || 500;
     //   if (error) {
